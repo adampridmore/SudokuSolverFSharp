c854ab9f916c4304d2ba959ea7d0d0834bf9b2dc
Adam Pridmore <adam_pridmore@hotmail.com>
2015-07-05 22:06:42 +0100

    - Extracted to a module.  - Now filters row solved cells.

--- SudokuFSharp/Script2.fsx
+++ SudokuFSharp/Script2.fsx
@@ -1,41 +1,10 @@
-﻿// #load "SudokuSolver.fs"
+﻿#load "SudokuSolver2.fs"
 #load "SudokuSolverHelpers.fs"
 #load "puzzles.fs"
 open SudokuSolverHelpers
-
-type Cell = 
-  | Solved of int
-  | Unsolved of int seq
-
-type Row = {
-  Cells : Cell array
-}
-
-
-type Puzzle = {
-  Rows : Row array
-} 
+open SudokuSolver2
   
 open puzzles
-
-let nl = System.Environment.NewLine
-
-let puzzleToPossibilities puzzle =
-  let toCell cell = 
-    match cell with 
-    | Some(cell) -> Solved(cell)
-    | _ -> Unsolved(seq{1..9})
-
-  let toRow cells =
-    {Row.Cells = (cells |> Seq.toArray )}
-
-  let toPuzzle rows =
-    {Puzzle.Rows = (rows |> Seq.toArray)}
-
-  puzzle 
-  |> Seq.map(fun row -> row |> Seq.map toCell)
-  |> Seq.map (fun cells -> cells |> toRow)
-  |> toPuzzle
 
 let printPossibilities (puzzle:Puzzle) = 
   let padd (s:string) = 
@@ -52,25 +21,23 @@ let printPossibilities (puzzle:Puzzle) =
     sprintf "|%s|" cellText
 
   let processRow (row:Row) = 
-    
-
     let line = row.Cells
                |> Seq.map printCellToString
                |> Seq.reduce (+)
-
     sprintf "||%s||%s" line nl
- 
+
+  printfn "" 
   puzzle.Rows
   |> Seq.map processRow
   |> Seq.reduce (+)
   |> printfn "%s"
 
-let solver (puzzle:int option [][]) =
-  puzzle 
-  |> puzzleToPossibilities
-  |> printPossibilities
+//let solver (puzzle:int option [][]) =
+//  puzzle 
+//  |> puzzleToPossibilities
+//  |> printPossibilities
 
-printfn ""
 puzzle3a 
-|> stringToPuzzle 
+|> stringToPuzzle
 |> solver
+|> printPossibilities
--- SudokuFSharp/SudokuSolver2.fs
+++ SudokuFSharp/SudokuSolver2.fs
@@ -1,1 +1,78 @@
-﻿
+﻿module SudokuSolver2
+
+type Cell = 
+  | Solved of int
+  | Unsolved of int seq
+
+type Row = {
+  Cells : Cell array
+}
+
+
+type Puzzle = {
+  Rows : Row array
+}
+
+let nl = System.Environment.NewLine
+
+let toCell cell = 
+  match cell with 
+  | Some(cell) -> Solved(cell)
+  | _ -> Unsolved(seq{1..9})
+
+let toRow cells =
+  {Row.Cells = (cells |> Seq.toArray )}
+
+let toPuzzle rows =
+  {Puzzle.Rows = (rows |> Seq.toArray)}
+
+let getRowCells (puzzle:Puzzle) rowIndex = 
+   puzzle.Rows.[rowIndex].Cells |> Seq.choose (fun cell -> match cell with 
+                                                           | Solved(v) -> Some(v)
+                                                           | _ -> None)
+
+//let getColumnCells (puzzle:int option [][]) columnIndex = 
+//  seq{0..8}
+//  |> Seq.map(fun currentRowIndex -> puzzle.[currentRowIndex].[columnIndex])
+//  |> Seq.toArray
+//
+//let getBlockCells (puzzle:int option [][]) cellx celly = 
+//  let blockIndexX = cellx / 3
+//  let blockIndexY = celly / 3
+//  seq{
+//    for x in 0..2 do 
+//      for y in 0..2 do
+//        yield puzzle.[blockIndexY*3+y].[blockIndexX*3+x]
+//  } |> Seq.toArray
+
+let puzzleToPossibilities puzzle =
+  puzzle 
+  |> Seq.map(fun row -> row |> Seq.map toCell)
+  |> Seq.map (fun cells -> cells |> toRow)
+  |> toPuzzle
+
+let filterPossibilities x y puzzle possibilities =
+  let eliminated = getRowCells puzzle y |> Set.ofSeq
+
+  Set.difference (possibilities |> Set.ofSeq) eliminated |> Set.toSeq
+
+
+let filterPuzzle (puzzle:Puzzle) =
+  let filterCell x y (cell:Cell) = 
+    match cell with
+    | Solved(v) -> Solved(v)
+    | Unsolved(possibilities) -> Unsolved(filterPossibilities x y puzzle possibilities)
+
+  let filterRow y (row:Row) =
+    row.Cells
+    |> Seq.mapi (fun x cell ->  filterCell x y cell)
+    |> toRow
+
+  puzzle.Rows 
+  |> Seq.mapi filterRow
+  |> toPuzzle
+
+let solver puzzleArarys =
+  let puzzle = puzzleArarys |> puzzleToPossibilities
+  
+  puzzle |> filterPuzzle
